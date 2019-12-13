61682ac6cea639c7700a0d71c257555f1cf0d334
yzh <295658413@qq.com>
2019-12-13 09:32:40 +0800

    新增公用的webView展示页面

--- app/src/main/java/com/example/mvvmapp/main/MainNewActivity.java
+++ app/src/main/java/com/example/mvvmapp/main/MainNewActivity.java
@@ -19,7 +19,6 @@ import com.example.commlib.weight.banner.BannerConfig;
 import com.example.commlib.weight.banner.Transformer;
 import com.example.mvvmapp.MainDetailActivity;
 import com.example.mvvmapp.R;
-import com.example.mvvmapp.TestWeightActivity;
 import com.example.mvvmapp.bean.WanAndroidBannerBean;
 import com.example.mvvmapp.databinding.ActivityNewMainBinding;
 import com.example.mvvmapp.viewmodel.MainNewViewModel;
